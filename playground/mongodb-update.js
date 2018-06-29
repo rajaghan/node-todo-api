1e69b18c8a1234bc0e0a7067a4a62567cb0f850c
Rajasekhar Ghanta <rajasekhar.ghanta@in.ibm.com>
2018-06-29 13:52:06 +0530

    update scripts

---
+++ playground/mongodb-update.js
@@ -0,0 +1,36 @@
+// const MongoClient = require('mongodb').MongoClient;
+//ES6 destructuring equivalent to above statement that is commented
+const {
+  MongoClient,
+  ObjectID
+} = require('mongodb');
+
+const mongoURL = 'mongodb://10.2.2.102:27017/TodoApp'
+
+//mongo v3 gets client instead of db
+MongoClient.connect(mongoURL, (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to mongodb server');
+  }
+  console.log('Connected to mongodb server');
+
+  //findOneAndUpdate(filter, update, options)
+  db.collection('Users').findOneAndUpdate({
+    state: 'karnataka'
+  }, {
+    $set: {
+      name: 'Rajasekhar Ghanta'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(JSON.stringify(result, undefined, 2));
+  }).catch((err) => {
+    console.log(err);
+  });
+
+  //db.close();
+});
