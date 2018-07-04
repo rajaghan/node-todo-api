55d85eeaf8f0b3ef6f5622a89e3a7b02229c5f53
Rajasekhar Ghanta <rajasekhar.ghanta@in.ibm.com>
2018-07-04 21:20:15 +0530

    fixing url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURL = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+const mongoURL = process.env.MONGODB_URI;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoURL);
